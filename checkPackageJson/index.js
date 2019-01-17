48449af6e973daca715dee8b0f51faab8528f5ff
Albert XU <xyb1114@gmail.com>
2019-01-17 18:15:04 +0100

    Update index.js

--- checkPackageJson/index.js
+++ checkPackageJson/index.js
@@ -1,21 +1,67 @@
+/*
+  la tâche pour détecter les mis à jours de node modules dans les 3 derniers jours
+  https://github.com/FnacDarty/fnac-pop/issues/2925
+*/
+
 const fs = require('fs')
 const path = require('path')
-const callApi = require('./callApi.js')
-const constants = require('./constants')
-const promisify = require('util').promisify
+const fetch = require('node-fetch')
+const { promisify } = require('util')
 
-ENCODING = constants.ENCODING
-GITHUB_LATEST_RELEASE_API = constants.GITHUB_LATEST_RELEASE_API
-GITHUB_SEARCH_API = constants.GITHUB_SEARCH_API
-millisecondsToDays = constants.millisecondsToDays
+const ENCODING = 'utf-8'
+const millisecondsToDays = millisecond =>
+  parseInt(millisecond / (1000 * 60 * 60 * 24), 10)
 
 const promisifyReadFile = promisify(fs.readFile)
 
-promisifyReadFile(
-  path.resolve(`${__dirname}/../`, 'package.json'),
-  ENCODING
-).then(data => {
-  const obj = JSON.parse(data)
-  const dependencies = Object.keys(obj.dependencies)
-  console.log('dependencies', dependencies)
+// pour récuperer les dependencies prod dans package.json
+async function getDependenciesFromPackageJson() {
+  const response = await promisifyReadFile(
+    path.resolve(`${__dirname}/../`, 'package.json'),
+    ENCODING
+  )
+  const dependencies = await Object.keys(JSON.parse(response).dependencies)
+  return dependencies
+}
+
+// helper fonction pour appel git api
+async function callApi(url) {
+  const options = {
+    path: url
+  }
+  const response = await fetch(url, options)
+  const data = await response.json()
+  return data
+}
+
+/**
+ *
+ * get the last update
+ *
+ * */
+async function getLatestUpdate(dependencies) {
+  const url = 'https://registry.npmjs.org/'
+  const result = await Promise.all(
+    dependencies.map(dependency =>
+      callApi(`${url}${dependency}`).then(response => {
+        const createdDate = new Date(response.time.modified)
+        const today = new Date()
+        if (millisecondsToDays(today - createdDate) < 3) {
+          console.log(dependency, 'is updated on', createdDate)
+          return true
+        }
+        return false
+      })
+    )
+  )
+  return result.includes(true)
+}
+
+// entry point
+getDependenciesFromPackageJson().then(dependencies => {
+  getLatestUpdate(dependencies).then(hasRecentUpdate => {
+    if (!hasRecentUpdate) {
+      console.log('There isn\'t recent update in the last 3 days.')
+    }
+  })
 })
